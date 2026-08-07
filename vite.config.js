28df36499768146ceda1bb2e72e13a2f9cc037ae
ewsstb <ewssitubondo@gmail.com>
2026-08-08 03:03:40 +0700

    Fix formatting in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/flood-ews-dashboard/', // ganti sesuai nama repository GitHub
-})
+  base: '/flood-ews-dashboard/',
+});
